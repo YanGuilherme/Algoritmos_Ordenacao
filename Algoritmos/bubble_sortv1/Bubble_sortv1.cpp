c61a1224f62b87ef673d46317ad4c932638b49e6
Yan <yanvzp@gmail.com>
2023-09-27 17:21:55 -0300

    ok

--- Algoritmos/bubble_sortv1/Bubble_sortv1.cpp
+++ Algoritmos/bubble_sortv1/Bubble_sortv1.cpp
@@ -7,13 +7,11 @@ BubbleSortv1::~BubbleSortv1(){}
 void BubbleSortv1::ordenar(DadosEntrada* entrada){
     double inicio = getTempoAtual();
     for (int i = 0; i < entrada->tamanho - 1; i++) {
-
         for (int j = 0; j < entrada->tamanho - i - 1; j++) {
             if (entrada->vector[j] > entrada->vector[j + 1]) {
                 int temp = entrada->vector[j];
                 entrada->vector[j] = entrada->vector[j + 1];
                 entrada->vector[j + 1] = temp;
-
             }
         }
     }
